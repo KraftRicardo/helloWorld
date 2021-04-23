bcb6eec5681c326190b633a9040923549dafc26c
Ricardo Kraft <kraftricardo@gmail.com>
2021-04-23 11:37:48 +0200

    Adding new project to Git

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+
+    // TODO
+    // short cut für generate code
+    // short cut für generate if, for, etc. loop
+
+    return 0;
+}
